5f7b3b777139b7a02a678187c71a31d264832e45
Erkka Lehtoranta <elehtora@hive.fi>
2022-09-25 05:45:47 +0300

    Fixed norm things, removed tests from release

--- includes/dispatch.h
+++ includes/dispatch.h
@@ -6,7 +6,7 @@
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 00:08:13 by elehtora          #+#    #+#             */
-/*   Updated: 2022/09/25 08:26:23 by elehtora         ###   ########.fr       */
+/*   Updated: 2022/09/25 08:34:13 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,11 @@
 
 // Dispatch table for conversion functions. The functions should take as
 // parameters (t_fstring *fs, va_list *ap) and return t_fstring *.
-typedef int (*t_convert)(t_fstring *fs, va_list *ap);
+typedef int	(*t_convert)	(t_fstring *fs, va_list *ap);
 
 // Global dispatch table. Duplicates cater to the indexing of fs->format (see:
 // ft_printf.h)
-static const t_convert	g_convert[CONVERSIONS] =
-{
+static const t_convert	g_convert[CONVERSIONS] = {
 	convert_signed_int,
 	convert_unsigned_int,
 	convert_unsigned_int,
